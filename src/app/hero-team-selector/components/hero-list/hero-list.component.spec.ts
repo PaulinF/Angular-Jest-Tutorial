4aea35d40ea9c80a3ebe16cdf192491d1615ca2f
PaulinF <49908514+PaulinF@users.noreply.github.com>
2019-05-14 15:09:03 +0200

    Update hero-list.component.spec.ts

--- src/app/hero-team-selector/components/hero-list/hero-list.component.spec.ts
+++ src/app/hero-team-selector/components/hero-list/hero-list.component.spec.ts
@@ -18,6 +18,28 @@ describe('HeroListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroListComponent);
     component = fixture.componentInstance;
+    component.title = 'hero';
+    component.list = [
+     {
+      id: 2,
+      name: 'Hulk',
+      real_name: 'Robert Bruce Banner',
+      thumb:
+        'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
+      image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+      description:
+        'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+    },
+    {
+      id: 3,
+      name: 'IronMan',
+      real_name: 'Anthony Edward "Tony" Stark',
+      thumb:
+        'http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55/standard_xlarge.jpg',
+      image: 'https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg',
+      description: ''
+    }
+  ];
     fixture.detectChanges();
   });
 
