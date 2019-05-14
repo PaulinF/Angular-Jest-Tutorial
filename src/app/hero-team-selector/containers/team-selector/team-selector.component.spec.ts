352b98ea243553a8a09ed29428175cec2060497f
PaulinF <49908514+PaulinF@users.noreply.github.com>
2019-05-14 15:56:58 +0200

    Update team-selector.component.spec.ts

--- src/app/hero-team-selector/containers/team-selector/team-selector.component.spec.ts
+++ src/app/hero-team-selector/containers/team-selector/team-selector.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HeroListComponent } from 'src/app/hero-team-selector/components/hero-list/hero-list.component';
+import { HeroListItemComponent } from 'src/app/hero-team-selector/components/hero-list-item/hero-list-item.component';
+import { TeamComponent } from 'src/app/hero-team-selector/components/team/team.component';
+import { HeroCardComponent } from 'src/app/hero-team-selector/components/hero-card/hero-card.component';
+import { EmptyCardComponent } from 'src/app/hero-team-selector/components/empty-card/empty-card.component';
 import { TeamSelectorComponent } from './team-selector.component';
 
 describe('TeamSelectorComponent', () => {
