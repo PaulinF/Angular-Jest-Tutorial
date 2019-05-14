efc19cdffd1df401b1891202e3c1508d052ac4cb
PaulinF <49908514+PaulinF@users.noreply.github.com>
2019-05-14 15:34:43 +0200

    Update team-selector.component.spec.ts

--- src/app/hero-team-selector/containers/team-selector/team-selector.component.spec.ts
+++ src/app/hero-team-selector/containers/team-selector/team-selector.component.spec.ts
@@ -3,12 +3,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TeamSelectorComponent } from './team-selector.component';
 
 describe('TeamSelectorComponent', () => {
-  let component: TeamSelectorComponent;
+  let component: TeamSelectorComponent,
+      HeroListComponent,
+      HeroListItemComponent,
+      TeamComponent,
+      HeroCardComponent,
+      EmptyCardComponent;
   let fixture: ComponentFixture<TeamSelectorComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TeamSelectorComponent,
+      declarations: [ 
+      TeamSelectorComponent,
       HeroListComponent,
       HeroListItemComponent,
       TeamComponent,
