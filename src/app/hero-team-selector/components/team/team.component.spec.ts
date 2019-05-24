f70b4823bbf15f82f0c2d0c06ee2417e83ec25d4
PaulinF <49908514+PaulinF@users.noreply.github.com>
2019-05-24 16:44:55 +0200

    Update team.component.spec.ts

--- src/app/hero-team-selector/components/team/team.component.spec.ts
+++ src/app/hero-team-selector/components/team/team.component.spec.ts
@@ -23,4 +23,23 @@ describe('TeamComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should emit event removeMember with formatted object memberToRemove', () => {
+    const member = {
+      id: 3,
+      name: 'IronMan',
+      real_name: 'Anthony Edward "Tony" Stark',
+      thumb:
+        'http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55/standard_xlarge.jpg',
+      image: 'https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg',
+      description: ''
+    };
+    component.onRemoveMember(member);
+    expect(component.removeMember.emit).toHaveBeenLastCalledWith({
+      type: component.title,
+      member
+    });
+    
+  });
+  
 });
